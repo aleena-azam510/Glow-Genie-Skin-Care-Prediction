86f3d9a0a351e1edebbd7347b5c9929e7057ee19
aleena-azam510 <aleenaazamchandio12@gmail.com>
2025-08-31 22:33:36 +0500

    added settings

--- skinpredictor/urls.py
+++ skinpredictor/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from reviews import views as review_views
-
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('predictor.urls')),
@@ -33,3 +33,7 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
